3448124fd0dc715d4d423f3a72c528d9b1bdaf04
Elin Robertson <robertsonelin@gmail.com>
2022-10-11 11:43:14 +0200

    testar lite

---
+++ script.js
@@ -0,0 +1,12 @@
+let rightBtn = document.querySelector(".choice1-right");
+const leftBtn = document.querySelector(".choice2-left");
+let h1 = document.querySelector("h1")
+
+rightBtn.addEventListener("click", right1func);
+
+function right1func() {
+const klappahund = document.createElement("button");
+klappahund.insertAdjacentElement("afterend", rightBtn)
+}
+
+
