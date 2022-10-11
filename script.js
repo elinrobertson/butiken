13ca24bfb71c72ccf535a553ec44eb3f7312d0d4
Sam Azimi <samzih1337@gmail.com>
2022-10-11 11:59:05 +0200

    dsfdsfsdf

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let rightBtn = document.querySelector(".choice1-right");
-const leftBtn = document.querySelector(".choice2-left");
+const leftBtn = document.querySelector(".choice1-left");
 let h1 = document.querySelector("h1")
+const klappahund = document.createElement("button");
 
 rightBtn.addEventListener("click", right1func);
 
 function right1func() {
-const klappahund = document.createElement("button");
-klappahund.insertAdjacentElement("afterend", rightBtn)
+klappahund.insertAdjacentElement("afterend", rightBtn);
 }
 
 
