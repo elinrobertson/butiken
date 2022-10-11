93bac1aaf93c84bf65ae600bed91c69a1879286b
Sam Azimi <samzih1337@gmail.com>
2022-10-11 14:07:12 +0200

    new branch for testing

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 let rightBtn = document.querySelector(".choice1-right");
-const leftBtn = document.querySelector(".choice1-left");
+let leftBtn = document.querySelector(".choice1-left");
 let h1 = document.querySelector("h1")
-const klappahund = document.createElement("button");
+let orText = document.querySelector(".orText");
 
-rightBtn.addEventListener("click", right1func);
+rightBtn.addEventListener("click", funcRightRight1);
 
-function right1func() {
-<<<<<<< HEAD
-klappahund.insertAdjacentElement("afterend", rightBtn);
-=======
-    rightBtn.textContent = "message";
-    // const klappahund = document.createElement("button");
-    // klappahund.insertAdjacentElement("afterend", rightBtn)
->>>>>>> fc7d1e7d458344cb89b9126bc2cca35c578f8be1
+function funcRightRight1() {
+    h1.textContent = "För att nå ICA så behöver du passera ett övergångsställe, men plötsligt så ser du en gullig hund!"
+
+    rightBtn.textContent = "Gå till övergångsställe";
+    leftBtn.textContent = "Klappa hunden";
+    rightBtn.addEventListener("click", funcRightRight2);
+    leftBtn.addEventListener("click", funcRightRight2);
 }
 
+
+
+
+rightBtn.addEventListener("click", funcRightLeft1);
+
+function funcRightLeft1() {
+    h1.textContent = "123"
+
+    rightBtn.textContent = "Gå till övergångsställe";
+    leftBtn.textContent = "Klappa hunden";
+    // rightBtn.addEventListener("click", funcRightRight3);
+}
+
+
+
+
+// function funcRightRight2() {
+//     h1.textContent = "Du är framme vid ICA!"
+//     rightBtn.textContent = "Gå hem";
+//     leftBtn.style.display = "none";
+//     orText.style.display = "none";
+//     rightBtn.addEventListener("click", function goHome() {
+//         h1.textContent = "Du är nu hemma igen :)"
+//         rightBtn.style.display = "none";
+//     });
+// }
 
